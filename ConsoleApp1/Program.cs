597db2023f48130b4f6caa7bd025e6ff1718cb9e
rawbin.zhang <rawbin.zhang@lunarax.io>
2022-02-24 20:28:59 +0800

    feat: 增加C# pdf导出demo

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class SeleniumDemo {
+    static void Main()
+    {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.AddArgument("--headless");
+        var driver = new ChromeDriver(chromeOptions);
+        
+        Console.WriteLine("start");
+        driver.Navigate().GoToUrl("http://www.baidu.com");
+        driver.Manage().Cookies.AddCookie(new Cookie("xxxtoken","xxxcokenValue",".baidu.com","/", new DateTime(2023,5,5)));
+
+        PrintOptions printOptions = new PrintOptions();
+        PrintDocument doc = driver.Print(printOptions);
+        try
+        {
+            doc.SaveAsFile("./csharp-selenium-out.pdf");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        Console.WriteLine("end");
+        driver.Quit();
+    }
+}
---
+++ ConsoleApp1/ReadMe.md
@@ -0,0 +1,4 @@
+添加依赖
+`dotnet add package Selenium.WebDriver`
+
+运行 `Program.cs`
